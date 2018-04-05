a709296198275949e641b6904a03bbef76e45e16
athulantonynp <athulantonynp@gmail.com>
2018-04-05 10:23:39 +0530

    add maps api

--- app/src/main/java/in/jumboradar/activities/Home.kt
+++ app/src/main/java/in/jumboradar/activities/Home.kt
@@ -4,12 +4,15 @@ import `in`.jumboradar.R
 import `in`.jumboradar.modules.LocationModule
 import `in`.jumboradar.utils.Constants
 import `in`.jumboradar.utils.Toasty
+import android.content.Intent
 import android.content.pm.PackageManager
 import android.location.Location
 import android.location.LocationListener
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import com.google.android.gms.common.GoogleApiAvailability
+import com.google.android.gms.common.api.GoogleApi
 import kotlinx.android.synthetic.main.activity_home.*
 
 class Home : AppCompatActivity(), LocationListener {
@@ -20,6 +23,7 @@ class Home : AppCompatActivity(), LocationListener {
         setContentView(R.layout.activity_home)
         btnHistory.setOnClickListener { onHistoryClick() }
         btnReport.setOnClickListener { onReportClick() }
+
     }
 
     private fun onReportClick() {
@@ -32,8 +36,10 @@ class Home : AppCompatActivity(), LocationListener {
     }
 
     private fun onHistoryClick() {
-
+        startActivity(Intent(Home@this,History::class.java))
     }
+
+
 
 
     override fun onStatusChanged(provider: String?, status: Int, extras: Bundle?) {
